7a53feccd10acb2e84d5bd4d9410925fd2dc5821
souvik1232 <souvik199831@gmail.com>
2021-01-11 14:19:50 +0530

    Added:Function to add details

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
 console.log("Welcome to Address Book");
+const address = require('./utility');
+address.addDetails();
---
+++ utility.js
@@ -0,0 +1,24 @@
+const readline = require('readline-sync');
+const fs = require('fs');
+class AddresBook {
+    person = {};
+    addDetails = () =>{
+       let fName = readline.question('Enter your First Name:') ;
+       this.person["FirstName"] = fName;
+       let lName = readline.question('Enter your Last Name:');
+       this.person["LastName"] = lName;
+       let addr = readline.question('Enter your address:');
+       this.person["Address"] = addr;
+       let zip = readline.question('Enter your Zip Code:');
+       this.person["Zip"] = zip;
+       let city = readline.question('Enter your city name:');
+       this.person["City"] = city;
+       let state = readline.question('Enter your state name:');
+       this.person["State"] = state;
+       let phone = readline.question('Enter your phone number:');
+       this.person["Phone"] = phone;
+       let data = (JSON.stringify(this.person)).concat("\n");
+       fs.writeFileSync('book.json',data,{encoding:"utf8",flag:"a"});
+    }
+}
+module.exports = new AddresBook();
